602745b24dc5d0187d36515007a57aca9af33e0f
TheGlink <c.johnston1994@hotmail.co.uk>
2022-11-14 20:08:44 +0000

    got sample data to base cleaning off

--- pinterest_proj/Consumers/batch_consumer.py
+++ pinterest_proj/Consumers/batch_consumer.py
@@ -2,18 +2,28 @@ from kafka import KafkaConsumer
 from datetime import datetime
 import pinterest_proj.config as c
 import json, boto3, os
+from pprint import pprint
 
 batch_consumer = KafkaConsumer(
     "PintrestData",
     bootstrap_servers="localhost:9092",
-    value_deserializer=lambda  x: json.loads(x.decode("utf-8"))  
-)
+    value_deserializer=lambda  x: json.loads(x.decode("utf-8"))
+    )
 
 s3_resource = boto3.resource('s3')
 date = datetime.now().strftime("%Y-%m-%d").split('-')
 
-for message in batch_consumer:
-    unique_id = message.value['unique_id'].replace('-', '_')
+for index, message in enumerate(batch_consumer):
+    print(index)
+    if index >= 25:
+        break
+    with open('data.txt', 'a') as f:
+        f.write(f'{message}\n')
+
+    
+
+    '''unique_id = message.value['unique_id'].replace('-', '_')
     path = os.path.join('raw_data', f'year={date[0]}', f'month={date[1]}', f'day={date[2]}', f'{unique_id}.json')
     s3_resource.Bucket(c.BUCKET_NAME).put_object(Body=json.dumps(message.value), Key=path)
-    break
+    if index >= 100:
+        break'''
