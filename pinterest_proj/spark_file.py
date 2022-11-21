86a83520400fffc1860d25ab8b66aa92a9ee2f85
TheGlink <c.johnston1994@hotmail.co.uk>
2022-11-21 21:42:16 +0000

    follower count cleaning + TODO

--- pinterest_proj/spark_file.py
+++ pinterest_proj/spark_file.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import regexp_replace, col
 from pyspark import SparkContext, SparkConf
-import os, boto3
+import os, boto3, pyspark
 import config as c
 
 # Adding the packages required to get data from S3  
@@ -8,7 +9,8 @@ os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages com.amazonaws:aws-java-sdk-s3:1.
 
 # Creating our Spark configuration
 conf = SparkConf() \
-    .setAppName('S3toSpark') \
+    .setMaster("local[*]") \
+    .setAppName('Pinterest_spark_app') \
 
 
 sc=SparkContext(conf=conf)
@@ -23,22 +25,27 @@ hadoopConf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoo
 # Create our Spark session
 spark=SparkSession(sc)
 
-#s3 = boto3.resource("s3").Bucket()
-
 # Read from the S3 bucket
 df = spark.read.json("s3a://pinterest-data-0759ba42-ccf0-4396-9b86-76de3b8e6640/raw_data/year=2022/month=11/day=14/*.json") # You may want to change this to read csv depending on the files your reading from the bucket
-df.show()
 
+'''
+Cleaning Data
+Convert k and M in follower count to 0's
+TODO - Title, Description, poster name, tag_list, img_src
+'''
 
-'''findspark.init()
+df = df.withColumn("follower_count", regexp_replace(col("follower_count"), "k", "000")) \
+    .withColumn("follower_count", regexp_replace(col("follower_count"), "M", "000000")) \
+    .withColumn("follower_count", col("follower_count").cast("int")) \
+    .show()
+    
 
-session = pyspark.sql.SparkSession.builder.config(conf=cfg).getOrCreate()
-sc = session.sparkContext.parallelize([1,2,3,4,5])
-print(sc.take(3))
-power_of_self = sc.map(lambda x: x**x)
-print(power_of_self.take(5))
+'''
+correct = .withColumn('follower_count', regexp_replace(col('follower_count'), 'k', '000')) 
+tried:
+df.follower_count
+col("follower_count")
+$"follower_count"
+df["follower_count"]
 
-result = (
-    sc.parallelize(dataC)
-    .filter(lambda val)
-)'''
+'''
