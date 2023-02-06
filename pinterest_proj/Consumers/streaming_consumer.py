ab6de62cbc813979a0f5f7aa659c1484411be783
TheGlink <c.johnston1994@hotmail.co.uk>
2023-02-06 22:12:44 +0000

    Streaming consumer cleaning sorted

--- pinterest_proj/Consumers/streaming_consumer.py
+++ pinterest_proj/Consumers/streaming_consumer.py
@@ -1,5 +1,6 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, types
 import pyspark.sql.functions as PysparkSQLFunctions
+from pyspark.sql.functions import regexp_replace
 from pyspark.sql.functions import col
 from pyspark.sql.types import StructField, StructType, StringType, IntegerType 
 import os
@@ -25,36 +26,84 @@ spark = SparkSession \
 # Set logging level
 spark.sparkContext.setLogLevel("ERROR")
 
-
 # Read stream from Kafka topic
 stream_df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
-    .option("subscribe", kafka_topic) \
-    .option("startingOffsets", "earliest") \
-    .load()
+            .readStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+            .option("subscribe", kafka_topic) \
+            .option("startingOffsets", "earliest") \
+            .load()
+
+def null_if_not_match(value:str, comparison:str):
+        '''
+        compares two strings, value and comparison
+        if value does not match comparison, value is returned
+        if value matches comparison the return will be None type
+        '''
+        return PysparkSQLFunctions.when(value != comparison, value).otherwise(PysparkSQLFunctions.lit(None))
 
 # Define the Schema to convert kafka data to a JSON object
 jsonSchema = StructType([StructField("index", IntegerType()),
-                        StructField("unique_id", StringType()),
-                        StructField("title", StringType()),
-                        StructField("description", StringType()),
-                        StructField("poster_name", StringType()),
-                        StructField("follower_count", StringType()),
-                        StructField("is_image_or_video", StructType()),
-                        StructField("image_src", StringType()),
-                        StructField("downloaded", IntegerType()),
-                        StructField("save_location", IntegerType()),
-                        StructField("category", IntegerType())
-                    ])
-# Stream Cleaning
+                         StructField("unique_id", StringType()),
+                         StructField("title", StringType()),
+                         StructField("description", StringType()),
+                         StructField("follower_count", StringType()),
+                         StructField("tag_list", StringType()),
+                         StructField("is_image_or_video", StringType()),
+                         StructField("image_src", StringType()),
+                         StructField("downloaded", IntegerType()),
+                         StructField("save_location", StringType()),
+                         StructField("category", StringType())
+                         ])
+
+to_null =  {"title" : "No Title Data Available",
+            "description":"No description available Story format",
+            "follower_count" : "User Info Error",
+            "tag_list" : "N,o, ,T,a,g,s, ,A,v,a,i,l,a,b,l,e",
+            "image_src" : "Image src error."}
+
+#cast kafka stream to string
 stream_df = stream_df.selectExpr("CAST(value as STRING)")
 
-stream_df = stream_df \
-            .withColumn("value", PysparkSQLFunctions \
+# Apply schema to stream data
+stream_df = stream_df.withColumn("value", PysparkSQLFunctions \
             .from_json(stream_df["value"], jsonSchema)) \
-            .select(col("value.*"))      
+            .select(col("value.*"))
+
+# Stream Cleaning
+# Drop duplicates and NA values
+stream_df = stream_df.dropDuplicates(subset=["unique_id", "title", "description", "tag_list"]) \
+                     .dropna(thresh=2, subset=["title", "description", "tag_list"])
+
+# Clean follower count
+stream_df = stream_df.withColumn("follower_count", regexp_replace("follower_count", "k", "000")) \
+                     .withColumn("follower_count", regexp_replace("follower_count", "M", "000000")) \
+                     .withColumn("follower_count", regexp_replace("follower_count", "B", "000000000")) 
+
+#cast follower type to int
+stream_df = stream_df.withColumn("follower_count", stream_df["follower_count"].cast(types.IntegerType()))
+
+# Replace all values in df that match the to_null dict values
+for key, value in to_null.items():
+    stream_df = stream_df.withColumn(key, null_if_not_match(col(key), value))
+
+#clean save locations string    
+stream_df = stream_df.withColumn("save_location", regexp_replace(col("save_location"), "Local save in", ""))
+
+#cast downloaded to bool
+stream_df = stream_df.withColumn("downloaded", stream_df["downloaded"].cast(types.BooleanType()))
+
+stream_df = stream_df.select(["unique_id",
+                              "title",
+                              "description",
+                              "follower_count",
+                              "tag_list",
+                              "is_image_or_video",
+                              "image_src",
+                              "downloaded",
+                              "save_location",
+                              "category"])
 
 # Stream output
 query = stream_df.writeStream \
--- pinterest_proj/main.py
+++ pinterest_proj/main.py
@@ -1,7 +1,8 @@
 import uuid
 import logging as log
+import os, time
 
 from API import project_pin_API as api
 
 if __name__ == '__main__':
-    print()
+    print("Starting project")
